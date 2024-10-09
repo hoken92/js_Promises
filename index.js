879630b085c0575535c1b61f1dcb9a66bba7af4f
hoken92 <hkenneth92@gmail.com>
2024-10-08 19:08:04 -0700

    fixed a then method

--- index.js
+++ index.js
@@ -9,68 +9,72 @@ function getUserData(id) {
   };
 
   // .then() version
-  // try {
-  //   // Creating the return object
-  //   let userData = {};
+  try {
+    // Creating the return object
+    let userData = {};
 
-  //   // Finds which db the id belongs to
-  //   central(id).then((db) => {
-  //     userData.id = id;
-  //     // Runs the db method from the dbs object from the  central promise value
-  //     dbs[db](id).then((info) => {
-  //       // For in loop to iterate thru objects and inserts into userData object
-  //       for (const item in info) {
-  //         userData[item] = info[item];
-  //       }
+    // Finds which db the id belongs to
+    central(id)
+      .then((db) => {
+        userData.id = id;
+        return db;
+      })
+      .then((dbInfo) => {
+        // Runs the db method from the dbs object from the  central promise value
+        dbs[dbInfo](id).then((info) => {
+          // For in loop to iterate thru objects and inserts into userData object
+          for (const item in info) {
+            userData[item] = info[item];
+          }
 
-  //       // userData.username = info.username;
-  //       // userData.website = info.website;
-  //       // userData.company = info.company;
-  //     });
+          // userData.username = info.username;
+          // userData.website = info.website;
+          // userData.company = info.company;
+        });
 
-  //     // Runs the vault function w/ the provided id
-  //     vault(id).then((info) => {
-  //       // For in loop to iterate thru objects and inserts into userData object
-  //       for (const item in info) {
-  //         userData[item] = info[item];
-  //       }
-  //       // userData.name = info.name;
-  //       // userData.email = info.email;
-  //       // userData.address = info.address;
-  //       // userData.phone = info.phone;
-  //       console.log(userData);
-  //     });
-  //   });
-  //   // Catches any errors that happen above
-  // } catch (err) {
-  //   console.log(err);
-  // }
+        // Runs the vault function w/ the provided id
+        vault(id).then((info) => {
+          // For in loop to iterate thru objects and inserts into userData object
+          for (const item in info) {
+            userData[item] = info[item];
+          }
+          // userData.name = info.name;
+          // userData.email = info.email;
+          // userData.address = info.address;
+          // userData.phone = info.phone;
+          console.log(userData);
+        });
+      });
+    // Catches any errors that happen above
+  } catch (err) {
+    console.log(err);
+  }
 
   // Async/Await version
-  async function getData(id) {
-    try {
-      let userData = {};
-      const dbNumber = await central(id);
+  // async function getData(id) {
+  //   try {
+  //     let userData = {};
+  //     const dbNumber = await central(id);
 
-      const dbInfo = await dbs[dbNumber](id);
+  //     const dbInfo = await dbs[dbNumber](id);
 
-      for (const item in dbInfo) {
-        userData[item] = dbInfo[item];
-      }
+  //     for (const item in dbInfo) {
+  //       userData[item] = dbInfo[item];
+  //     }
 
-      const vaultInfo = await vault(id);
+  //     const vaultInfo = await vault(id);
 
-      for (const item in vaultInfo) {
-        userData[item] = vaultInfo[item];
-      }
+  //     for (const item in vaultInfo) {
+  //       userData[item] = vaultInfo[item];
+  //     }
 
-      console.log(userData);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  // Runs the async function
-  getData(id);
+  //     console.log(userData);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // // Runs the async function
+  // getData(id);
 }
 
 // In range id number
